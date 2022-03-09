4e91a0109635ccc97b8bd034f580795922dcb0b4
meteora277 <justin.s.diaz@gmail.com>
2022-03-09 09:23:10 -0500

    reference in memory db

--- src/main/java/com/example/cardapi/service/CardService.java
+++ src/main/java/com/example/cardapi/service/CardService.java
@@ -3,10 +3,16 @@ package com.example.cardapi.service;
 import com.example.cardapi.dao.CardDao;
 import com.example.cardapi.model.Card;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CardService {
   private final CardDao cardDao;
   
-  public CardService(CardDao cardDao) {
+  @Autowired
+  public CardService(@Qualifier("cardDao") CardDao cardDao) {
     this.cardDao = cardDao;
   }
 
