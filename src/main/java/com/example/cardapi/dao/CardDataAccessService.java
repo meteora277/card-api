39a2e0be770726b50c405e08cde9deca9cf617fa
meteora277 <justin.s.diaz@gmail.com>
2022-03-09 09:22:31 -0500

    create temporary in memory database

--- src/main/java/com/example/cardapi/dao/CardDataAccessService.java
+++ src/main/java/com/example/cardapi/dao/CardDataAccessService.java
@@ -1,10 +1,13 @@
 package com.example.cardapi.dao;
 import com.example.cardapi.model.Card;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Repository("cardDao")
 public class CardDataAccessService implements CardDao {
 
 private static List<Card> DB = new ArrayList<>();
